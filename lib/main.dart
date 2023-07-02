2ed81c201240c35e650290d6cef55d711cbdfe4e
Eren Bekar <92184715+ernbkar@users.noreply.github.com>
2023-07-02 23:45:52 +0300

    Add files via upload
    MEMAPP codes (only lib file included)

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:memapp/pages/profilepage.dart';
+import 'package:memapp/pages/loginpage.dart';
+import 'package:memapp/pages/samplepage.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black),
+        )
+      ),
+          home: ProfilePage(),
+          debugShowCheckedModeBanner: false,
+          );
+  }}
